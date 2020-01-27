ca042c60740276f27bfb4059cafdc1d6811eabb8
Kuruba Mahender <kuruba.mahender@nagarro.com>
2020-01-28 00:41:40 +0530

    Intial Commit...

---
+++ DatingApp.API/DatingApp.API/Migrations/20200127143704_IntialCreate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class IntialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tblValues",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblValues", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tblValues");
+        }
+    }
+}
---
+++ DatingApp.API/DatingApp.API/Model/Values.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Model
+{
+    public class Values
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+    }
+}
